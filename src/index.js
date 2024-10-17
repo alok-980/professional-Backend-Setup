db3af17dd466825ad2a056a21e2a65d2be42649d
Alok Chauhan <alokchuhan7896@gmail.com>
2024-10-17 13:47:56 +0530

    add utils file

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("error: ", error);
+        throw err  
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listening on port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed !!!", err)
+})
